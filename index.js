20486e4a1ed2114ad8f5278ab5ac6a22021f8351
Srinivasan Velu <40140@hexaware.com>
2018-05-29 10:55:19 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + "/public")));
+app.get("/", function (req, res) {
+    console.log("GET");
+    res.sendFile(__dirname+'/public/welcome.vxml');
+})
+app.get("/test", function (req, res) {
+    res.sendFile(__dirname+'/public/test.vxml');
+})
+app.post("/", function (req, res) {
+    console.log("POST");
+    res.contentType('application/xml');
+    res.sendFile(__dirname+'/public/welcome.vxml');
+})
+app.listen(process.env.PORT||3000);
